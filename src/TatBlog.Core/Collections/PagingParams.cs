d85d412ceeadcd12246871db9f6ef8fce297f910
RyunVu <ryun3980@gmail.com>
2023-03-16 17:28:52 +0700

    Added admin posts pager

---
+++ src/TatBlog.Core/Collections/PagingParams.cs
@@ -0,0 +1,10 @@
+﻿using TatBlog.Core.Contracts;
+
+namespace TatBlog.Core.Collections {
+    public class PagingParams : IPagingParams {
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+        public string SortColumn { get; set; } = "Id";
+        public string SortOrder { get; set; } = "DESC";
+    }
+}
