b7470c0f3bdcc2fae0b9045ea75196f83fbf2c59
RyunVu <ryun3980@gmail.com>
2023-03-17 15:55:02 +0700

    Added methods CRUD tag on admin web page

---
+++ src/TatBlog.WebApp/Areas/Admin/Models/TagFilterModel.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel;
+
+namespace TatBlog.WebApp.Areas.Admin.Models {
+    public class TagFilterModel {
+        [DisplayName("Từ khóa")]
+        public string Keyword { get; set; }
+    }
+}
