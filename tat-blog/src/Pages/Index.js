db9ae15c57701b4a72964bb5fe423d52f5ff4946
RyunVu <ryun3980@gmail.com>
2023-04-01 18:08:39 +0700

    Fetched Api success

--- tat-blog/src/Pages/Index.js
+++ tat-blog/src/Pages/Index.js
@@ -1,18 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import PostItem from '../Components/PostItem';
+import { getPosts } from '../Services/BlogRepository';
 
 const Index = () => {
     const [postsList, setPostsList] = useState([]);
 
     useEffect(() => {
         document.title = 'Trang chủ';
-    }, []);
 
-    if (postsList.length > 0)
+        getPosts().then((data) => {
+            if (data) {
+                setPostsList(data.items);
+            } else setPostsList([]);
+        });
+    }, []);
+    console.log(postsList);
+    if (postsList)
         return (
             <div className="p-4">
-                {postsList.map((item) => {
-                    return <PostItem postItem={item} />;
+                {postsList.map((item, index) => {
+                    return <PostItem postItem={item} key={index} />;
                 })}
             </div>
         );
