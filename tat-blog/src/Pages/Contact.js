06f4bd421e874d9f024c24a7e0c41012f89117a4
RyunVu <ryun3980@gmail.com>
2023-04-01 14:29:14 +0700

    Created React project for tat-blog

---
+++ tat-blog/src/Pages/Contact.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+
+const Contact = () => {
+    useEffect(() => {
+        document.title = 'Trang liên hệ';
+    }, []);
+    return <h1>Đây là trang liên hệ</h1>;
+};
+
+export default Contact;
