72216e99de73d036eb23ffd8f5180f29b631d509
RyunVu <ryun3980@gmail.com>
2023-04-09 19:58:17 +0700

    added navigation for admin page

--- tat-blog/src/App.jsx
+++ tat-blog/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-pascal-case */
 import './App.css';
 import Layout from './Pages/Layout';
 import Index from './Pages/Index';
@@ -8,6 +9,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import AdminLayout from './Components/Admin/Layout';
 import * as AdminIndex from './Pages/Admin/Index';
+import Authors from './Pages/Admin/Authors';
+import Categories from './Pages/Admin/Categories';
+import Comments from './Pages/Admin/Comments';
+import Posts from './Pages/Admin/Posts';
+import Tags from './Pages/Admin/Tags';
 
 function App() {
     return (
@@ -23,6 +29,11 @@ function App() {
 
                 <Route path="/admin" element={<AdminLayout />}>
                     <Route path="/admin" element={<AdminIndex.default />} />
+                    <Route path="/admin/authors" element={<Authors />} />
+                    <Route path="/admin/categories" element={<Categories />} />
+                    <Route path="/admin/comments" element={<Comments />} />
+                    <Route path="/admin/posts" element={<Posts />} />
+                    <Route path="/admin/tags" element={<Tags />} />
                 </Route>
             </Routes>
         </Router>
