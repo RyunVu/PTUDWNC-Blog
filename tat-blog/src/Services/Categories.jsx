fb1f9ff1f8b372b93bbb1d0db13f8311310b3935
RyunVu <ryun3980@gmail.com>
2023-04-25 06:13:14 +0700

    Update Categories.jsx

--- tat-blog/src/Services/Categories.jsx
+++ tat-blog/src/Services/Categories.jsx
@@ -13,3 +13,35 @@ export async function getCategories() {
     if (data.isSuccess) return data.result;
     else return null;
 }
+
+export async function getCategoriesByQueries(queries) {
+    const { data } = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/categories?${queries}`);
+
+    if (data.isSuccess) return data.result;
+    else return null;
+}
+
+export async function deleteCategoryById(id) {
+    const { data } = await axios.delete(`${process.env.REACT_APP_API_ENDPOINT}/categories/${id}`);
+
+    return data;
+}
+
+export async function getCategoryById(id = 0) {
+    const { data } = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/categories/${id}`);
+
+    if (data.isSuccess) return data.result;
+    else return null;
+}
+
+export async function createCategory(category) {
+    const { data } = await axios.post(`${process.env.REACT_APP_API_ENDPOINT}/categories`, category);
+
+    return data;
+}
+
+export async function updateCategory(id, category) {
+    const { data } = await axios.put(`${process.env.REACT_APP_API_ENDPOINT}/categories/${id}`, category);
+
+    return data;
+}
