5ac437c6e9ac8e4eaff665034ee22dd92043a33d
RyunVu <ryun3980@gmail.com>
2023-04-25 06:12:57 +0700

    Update AuthorFilterPane.jsx

--- tat-blog/src/Components/Admin/AuthorFilterPane.jsx
+++ tat-blog/src/Components/Admin/AuthorFilterPane.jsx
@@ -2,27 +2,50 @@ import { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-export default function AuthorFilterPane({ setName }) {
-    // Component's refs
+const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+export default function AuthorFilterPane({ setName, setYear, setMonth }) {
     const nameRef = useRef();
-    // Component's event handlers
+    const yearRef = useRef();
+    const monthRef = useRef();
+
     const handleFilterAuthors = (e) => {
         e.preventDefault();
         setName(nameRef.current.value);
+        setYear(yearRef.current.value);
+        setMonth(monthRef.current.value);
     };
 
     const handleClearFilter = () => {
         setName('');
+        setYear('');
+        setMonth('');
         nameRef.current.value = '';
+        yearRef.current.value = '';
+        monthRef.current.value = '';
     };
 
     return (
         <Form method="get" onSubmit={handleFilterAuthors} className="row gx-3 gy-2 align-items-center py-2">
             <Form.Group className="col-auto">
                 <Form.Label className="visually-hidden">Tên tác giả</Form.Label>
-                <Form.Control ref={nameRef} type="text" placeholder="Nhập tên tác giả..." name="keyword" />
+                <Form.Control ref={nameRef} type="text" placeholder="Nhập tên tác giả..." name="name" />
             </Form.Group>
-
+            <Form.Group className="col-auto">
+                <Form.Label className="visually-hidden">Nhập năm</Form.Label>
+                <Form.Control ref={yearRef} type="text" placeholder="Nhập năm..." name="year" />
+            </Form.Group>
+            <Form.Group className="col-auto">
+                <Form.Label className="visually-hidden">Tháng</Form.Label>
+                <Form.Select ref={monthRef} title="Tháng" name="month">
+                    <option value="">-- Chọn tháng --</option>
+                    {months.map((month) => (
+                        <option key={month} value={month}>
+                            Tháng {month}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
             <Form.Group className="col-auto">
                 <Button variant="primary" type="submit">
                     Tìm/Lọc
